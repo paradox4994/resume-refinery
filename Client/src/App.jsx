a0f4f56be91d6e00371533a2cec6ce2fb020a4fe
paradox4994 <ryanpereira499@gmail.com>
2024-04-17 16:27:45 -0400

    Added tailwind

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <>
-      <h1>Hi React</h1>
+      <h1 className="text-3xl font-bold underline text-center text-red-500 ">Hi React + Tailwind</h1> 
     </>
   )
 }
